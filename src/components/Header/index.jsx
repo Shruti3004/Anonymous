0f3756a924e42623767bd3fb908c016b0c8e14c8
Shruti3004 <mail2shruti.ag@gmail.com>
2021-10-07 00:28:49 +0530

    fixed the bugs
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -29,12 +29,25 @@ const Header = () => {
           ANON<span className="text-primaryColor">ymous</span>
         </h1>
         <img src={hero} alt="" className="mt-3 px-5" height="300px" />
-        <h4 className="text-center text-white mt-4">
-          Roadmap
-        </h4>
+        <h4 className="text-center text-white mt-4">Roadmap</h4>
         <div className="font-17 text-tertiaryColor mb-2">Look into future</div>
       </div>
-
+      <div className="container">
+      <div className="row mobile-display-none" style={{ marginTop: "60vh" }}>
+        <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+          <i className="fa fa-twitter fa-3x text-primaryColor p-2 icons-background"></i>
+          <div className="font-bold text-tertiaryColor font-25 mt-2">Twitter</div>
+          <div className="font-frekle text-tertiaryColor font-20">Follow our twitter for sneak Peeks</div>
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+          <div className="text-right">
+          <i className="fab fa-discord fa-3x text-primaryColor p-2 icons-background"></i>
+          <div className="font-bold text-tertiaryColor font-25 mt-2">Discord</div>
+          <div className="font-frekle text-tertiaryColor font-20">Join the distord for Latest updates</div>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   );
 };
